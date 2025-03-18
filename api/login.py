29aeb1cfbfcaa5246aa90d98dc9101a9612ee179
Rexi <diveinthewaveexploits@gmail.com>
2025-03-18 16:59:01 +0100

    Create login.py

---
+++ api/login.py
@@ -0,0 +1,9 @@
+# Logs If The File Is Opened
+
+import os
+
+def login_message():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Logging in cheatsense...")
+if __name__ == "__main__":
+    login_message()
